e2615e0cff1ceacd239e31fb0716bd6be0b74baf
Artem Reshilov <dtltasu@Artems-MacBook-Pro.local>
2021-06-24 14:27:13 +0400

    fixed

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -5,6 +5,7 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongod
 
 
 sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-get install -y mongodb-org
 
 sudo systemctl start mongod && sudo systemctl enable mongod
