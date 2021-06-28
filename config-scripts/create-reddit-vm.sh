e6d68402c76a2666eb7355e6b7e5ec96cebab04c
Artem Reshilov <dtltasu@Artems-MacBook-Pro.local>
2021-06-28 13:48:25 +0400

    changed files

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Create instance reddit
-yc compute instance create --name reddit-app --hostname reddit-app --memory=4  --create-boot-disk name=reddit-full,size=20,image-id=fd8ql51b7umepb11ccat --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 --metadata serial-port-enable=1 --ssh-key ~/.ssh/appuser.pub
+yc compute instance create --name reddit-app --hostname reddit-app --memory=4  --create-boot-disk name=reddit-full,size=20,image-id=fd8h74hlvvkq0k7k98th --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 --metadata serial-port-enable=1 --ssh-key ~/.ssh/appuser.pub
--- packer/scripts/install_reddit.sh
+++ packer/scripts/install_reddit.sh
@@ -1,5 +1,15 @@
-sudo apt-get update
-sudo apt-get install -y git
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y git
+apt-get install -y policykit-1
+sudo chmod -R 0777 /opt
+
+cd /opt
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
-puma -d
+
+mv -f /tmp/puma.service /etc/systemd/system/puma.service
+systemctl daemon-reload
+systemctl enable puma.service
+systemctl start puma.service
