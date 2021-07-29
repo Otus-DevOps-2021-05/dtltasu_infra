fa0003d93cb2c11f6ce0f1f45ca0cb9c7ce24989
optimax <optimax@kepler.local>
2021-07-29 11:51:11 +0400

    fixed

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,13 +1,13 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
+#  }
+#}
 
-provider "yandex-cloud" {
-  version                  = "0.47"
+provider "yandex" {
+  version                  = "0.35"
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
